70f0f4893633bfdb5ddc2c9919b415eb654fc353
student_bj <330318@student.mboutrecht.nl>
2021-02-05 10:39:34 +0100

    filled all docoments with text
    type: added the text to finish my part
    change: filled in the php code with text
    wy: time!!!!!

--- gameversions.php
+++ gameversions.php
@@ -49,14 +49,36 @@
 
     <!--Main div for text-->
     <div id="Main_body_text">
-      <h1 class="Paragrap_title">Titel hier</h1>
-      <p class="Paragraph_text">text hier</p>
+      <h1 class="Paragrap_title">Minecraft releases </h1>
+      <p class="Paragraph_text">Sinds dat minecraft is uitgekomen zijn er heel veel verschillende testversies uitgekomen en daarna de echte release die je dan kan  downloaden om het te gaan spelen. 
+      Minecraft java editie is verkrijgbaar voor 23,95 eurocent, zodra je de game hebt gekocht kan je singleplayer en multiplayer spelen via de alleenstaande Minecraft launcher. 
+      Er is ook een officiële demo die gratis is om te spelen maar er zit een tijd limiet op van 90 minuten.
+      </p>
+      <br>
+      <h1 class="Paragrap_title">alle releases</h1>
+      <p class="Paragraph_text">1.0 - November 18, 2011
+      1.1 - January 12, 2012
+      1.2.1 - March 1, 2012
+      1.3.1 - August 1, 2012
+      1.4.2 - The Pretty Scary Update - October 25, 2012
+      1.5 - Redstone Update - March 13, 2013
+      1.6 - The Horse Update - July 1, 2013
+      1.7 - The Update That Changed The World - October 25, 2013
+      1.8 - The Bountiful Update - September 2, 2014
+      1.9 - The Combat Update - February 29, 2016
+      1.10 - The Frostburn Update - June 8, 2016
+      1.11 - The Exploration Update - November 14, 2016
+      1.12 - The World of Color Update - June 7, 2017
+      1.13 - The Update Aquatic - July 18, 2018
+      1.14 - Village and Pillage - April 23, 2019
+      1.15 - Buzzy Bees - December 10, 2019
+      1.16 - Nether Update - June 23, 2020
+      1.17 - Caves and Cliffs - 2021
+      </p>
       <br>
       <h1 class="Paragrap_title">Titel hier</h1>
-      <p class="Paragraph_text">text hier</p>
-      <br>
-      <h1 class="Paragrap_title">Titel hier</h1>
-      <p class="Paragraph_text">text hier</p>
+      <p class="Paragraph_text">text hier      
+      </p>
       <br>
       <h1 class="Paragrap_title">Titel hier</h1>
       <p class="Paragraph_text">text hier</p>
